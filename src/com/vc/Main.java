d7f8de5280abf6bfa7c5e97a932cad0fb1fa3f6a
kknd714 <admin@rpg.ren>
2021-06-15 14:47:40 +0800

    init

---
+++ src/com/vc/Main.java
@@ -0,0 +1,12 @@
+package com.vc;
+
+/**
+ * author: VC
+ * create: 2021/6/15 14:47
+ * version: 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
